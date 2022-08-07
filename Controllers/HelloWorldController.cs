a6e65f1627e2dc838fc06fa10e2f328668dd7877
Eduado Gomez <egomezr@outlook.com>
2022-08-07 11:22:25 +0200

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using FirstMVCApp.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers;
+public class HelloWorldController : Controller
+{
+
+    static readonly List<DogViewModel> dogs = new();
+    public IActionResult Index()
+    {
+        return View(dogs);
+    }
+
+    public IActionResult Create()
+    {
+        var dogVM = new DogViewModel();
+        return View(dogVM);
+    }
+
+    public IActionResult CreateDog(DogViewModel viewModel)
+    {
+        dogs.Add(viewModel);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public string Hello()
+    {
+        return "whose theree";
+    }
+
+
+
+}
---
+++ Models/DogViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstMVCApp.Models;
+
+public class DogViewModel
+{
+    public string? Name
+    {
+        get; set;
+    }
+    public int? Age
+    {
+        get; set;
+    }
+}
